34a067178ff87d98795ae18f3a9d14b85d55139d
Rob Huzzey <robert.huzzey@holidayextras.com>
2019-08-23 13:57:43 +0100

    my attempt

--- challenge.js
+++ challenge.js
@@ -1,5 +1,11 @@
 const unobstructedWestViewCount = buildings => {
-  throw new Error('implement me!')
+  return buildings.reverse().reduce((acc, curr) => {
+    if(curr > acc.lowest) {
+    acc.count = acc.count +1
+    acc.lowest = curr
+    }
+    return acc
+  }, {lowest: 0, count: 0}).count
 }
 
 module.exports = { unobstructedWestViewCount }
